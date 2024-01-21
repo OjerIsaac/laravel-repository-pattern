3142daa1f01b22ffaa27775ed1ec4a3014467899
OjerIsaac <isaacojerumu@gmail.com>
2024-01-22 00:07:45 +0100

    feat: user intefaces & repository

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use App\Repository\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{   
+
+    public function store($data = [] )
+    {   
+        //store the user
+    }
+    
+}
